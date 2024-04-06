d8d1762d544de55139561ceb906db59634112b63
Lucas Quaresma <lucas.quaresma1@gmail.com>
2024-04-05 21:55:13 -0300

    first implementations

---
+++ src/lists.py
@@ -0,0 +1,28 @@
+def unpack_nested_lists(nlst, mode="comprehension"):
+    """
+    This takes in unordered and unstrucured nested lists and flattens everything.
+    Example: 
+        Input:  [0, 1, [2, 3], [4, [5, [6, 7], 8], 9]]
+        Output: [0 ,1, 2, 3, 4, 5, 6, 7, 8, 9]
+    """
+    match mode:
+        case "comprehension":
+            return [
+                item 
+                for sublist in nlst 
+                for item in (
+                    unpack_nested_lists(sublist) 
+                    if isinstance(sublist, list)
+                    else [sublist]
+                    )
+                ]
+        case "loop":
+            flist = []
+            for item in nlst:
+                if isinstance(item, list):
+                    flist.extend(unpack_nested_lists(item, mode="loop"))
+                else:
+                    flist.append(item)
+            return flist
+        case _:
+            raise ValueError("Invalid list unpacking method!")
